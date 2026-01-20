b34a4654d21b223e0c15b7ef5f43e77816405a27
kadar bache <khadarpaashe123@gmail.com>
2026-01-20 17:18:00 +0300

    feat: updated the parcel version

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '66c65fe1-34bb-4fb6-97b6-aabb8d783e40';
+export const KEY = '272d5fc5-edea-402c-b6bd-ca603770fcee';
 export const MODEL_CLOSE_SEC = 2.5;
